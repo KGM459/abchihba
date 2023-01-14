4d892063b1ce1d3e7af68444f6fd7f9fd622fedf
kgmCDLIX <dibildaunov@gmail.com>
2023-01-14 20:06:14 +0500

    Выполнил задание 2

---
+++ Task05/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача с n=4 на выходе все целые числа в промежутке от N до -N
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int count = -number;
+
+while (count <= number)
+    {
+    Console.Write($"{count} ");
+    count++;
+    }
+}
+else
+{
+    Console.WriteLine("Введено не натуральное число!");
+}
---
+++ TaskTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿// написать программу которая:
+// 1. на входе берет два числа
+// 2. на выходе показывает большее число 
+ Console.Write("Введите число 1: ");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+ Console.Write("Введите число 2: ");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число {numberA} > {numberB}");
+}
+else 
+{
+    Console.WriteLine($"Число {numberB} > {numberA}");
+}
