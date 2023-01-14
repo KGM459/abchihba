efe1197974037c9e4c8f56b3267c3de45d8a2d80
kgmCDLIX <dibildaunov@gmail.com>
2023-01-14 21:44:52 +0500

    Я ЗАКОНЧИЛ!!!

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая:
+// 1. На вход принимает число n
+// 2. На выходе показывает все четные числа от 1 до N
+
+Console.Write("введите число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= N)
+{ 
+    if (count % 2 == 0)
+    {     
+    Console.Write($"{count} ");
+    }
+    count++;
+ }
+
