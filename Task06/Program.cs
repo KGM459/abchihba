891c2e248c872fe91176a6f12051f75d7143f88d
kgmCDLIX <dibildaunov@gmail.com>
2023-01-14 21:24:06 +0500

    Решено заданание 6!

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать программу, которая:
+// 1. На вход принимает число
+// 2. На выход отвечает, четное ли это число или нет
+
+Console.Write("Хочешь узнать, является ли число четным или нет? введи его:  ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.WriteLine($"Число {numberA} - Четное!");
+}
+else 
+{
+    Console.WriteLine($"Число {numberA} - Не четное!");
+}
