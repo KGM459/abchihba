c1c91f44ea8c74f596073f7a37b3181e4e37e4d1
kgmCDLIX <dibildaunov@gmail.com>
2023-01-14 21:29:06 +0500

    Решено заданание 6, по новой!!!

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -2,13 +2,14 @@
 // 1. На вход принимает число
 // 2. На выход отвечает, четное ли это число или нет
 
-Console.Write("Хочешь узнать, является ли число четным или нет? введи его:  ");
+
+Console.Write("введите число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 if (numberA % 2 == 0)
 {
-    Console.WriteLine($"Число {numberA} - Четное!");
+    Console.WriteLine($"Да");
 }
 else 
 {
-    Console.WriteLine($"Число {numberA} - Не четное!");
+    Console.WriteLine($"Нет");
 }
