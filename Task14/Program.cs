e7fdc1296d76624c7f0326d4d3909405d9c0a0f7
kgmCDLIX <dibildaunov@gmail.com>
2023-01-21 17:56:46 +0500

    коммит сем2 2:35

---
+++ Task14/Program.cs
@@ -0,0 +1,22 @@
+﻿// НАписать программу, которая:
+// на вход принимает число
+// на выход проверяет, кратно ли оно одновременно 7 и 23
+
+Console.WriteLine ("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if ( num % 7 == 0)
+{
+ if ( num % 23 == 0)
+ {
+    Console.WriteLine ($"Число {num} кратно 7 и 23");
+ }
+ else 
+{
+   Console.WriteLine ($"Число {num} не кратно 7 и 23"); 
+}
+}
+else
+{
+Console.WriteLine ($"Число {num} не кратно 7 и 23"); 
+}
+
--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,3 +1,15 @@
-﻿// НАписать программу, которая:
-// на вход принимает два числа
-// на выход проверяет является ли одно из них квадратом другого
+﻿bool Square(int number1, int number2)
+{
+return ((number1 * number1 == number2) || (number2 * number2 == number1));
+}
+
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = Square(number1, number2);
+
+if (result == true) Console.WriteLine("да");
+else Console.WriteLine("нет");
