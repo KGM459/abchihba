7d76b432ce6e40da803f7a0271e1863030664055
kgmCDLIX <dibildaunov@gmail.com>
2023-01-21 18:54:07 +0500

    семинар два выполнил задачу 13

---
+++ Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// на вход: принимает число
+//на выход: показывает третью цифру или говорит что ее нет.
+
+Console.WriteLine ("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+ Console.WriteLine("третьей цифры нет");
+}
+ while (num > 1000)
+ {
+    num = num / 10;
+ }
+ int ThirdDigit = num % 10;
+Console.WriteLine($"{ThirdDigit}");
