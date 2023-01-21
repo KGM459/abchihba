2e3af6e51059a2bacfeb30a3fb85392c6d25b88b
kgmCDLIX <dibildaunov@gmail.com>
2023-01-21 19:16:54 +0500

    ДОМАШНЕЕ ЗАДАНИЕ СЕМИНАР 2 ГОТОВо

---
+++ Task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// на вход принимает число
+// на выход говорит является ли день недели под этим числом выходным
+Console.WriteLine("Введите число от 1 до 7");
+int day = Convert.ToInt32(Console.ReadLine());
+if ( day > 7 )
+{
+    Console.WriteLine("Введенно неправильное число");
+}
+if ( day < 1 )
+{
+    Console.WriteLine("Введенно неправильное число");
+}
+if ( day <= 5)
+{
+    Console.WriteLine("Нет");
+}
+if ( day > 5)
+{
+    Console.WriteLine("Да");
+}
+
+
