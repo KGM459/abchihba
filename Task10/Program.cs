b013eea99fb2e67d8c04068971e3bff8a4283682
kgmCDLIX <dibildaunov@gmail.com>
2023-01-21 18:33:52 +0500

    дз первое задание  выполнил

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая:
+// на вход принимает трехзначное число
+// на выходе показывает вторую цифру  этого числа
+
+Console.WriteLine ("Введите число от 100 до 999");
+int num = Convert.ToInt32(Console.ReadLine());
+if ( num > 999)
+{
+    Console.WriteLine ("Введенно неправильное число");
+}
+if ( num < 100)
+{
+    Console.WriteLine ("Введенно неправильное число");
+}
+int SecondDigit = num % 100 / 10 ;
+Console.WriteLine ($"{SecondDigit}");
